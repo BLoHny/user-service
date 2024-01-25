2fe273c7dc1ff0d5ff28101b82b39468ee6affa0
BLoHny <113517233+BLoHny@users.noreply.github.com>
2024-01-25 21:30:12 +0900

    feign exception 처리

--- src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/impl/UserServiceImpl.java
@@ -6,21 +6,20 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.entity.User;
 import com.example.userservice.repository.UserRepository;
 import com.example.userservice.service.UserService;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -76,7 +75,12 @@ public class UserServiceImpl implements UserService {
                         new ParameterizedTypeReference<>() {
                 });*/
 
-        List<ResponseOrder> list = client.getOrders(userId);
+        List<ResponseOrder> list = null;
+        try {
+            list = client.getOrders(userId);
+        } catch (FeignException ex) {
+            log.error(ex.getMessage());
+        }
 
         userDto.setOrders(list);
 
